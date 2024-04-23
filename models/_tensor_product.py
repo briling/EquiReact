de131704707d1df7d1c747598a88ac346667da09
Ksenia <ksenia.briling@epfl.ch>
2024-04-23 03:31:29 +0200

    Simplify compilation

--- models/_tensor_product.py
+++ models/_tensor_product.py
@@ -1,89 +1,30 @@
-import warnings
 from math import sqrt
-
 import torch
-from torch import fx
 
-from e3nn.util.codegen import CodeGenMixin
-from e3nn.util.jit import compile_mode
-
-
-
-def codegen_tensor_product_left_right(n_s, path_weight) -> fx.GraphModule:
-    graph = fx.Graph()
-
-    # = Function definitions =
-    tracer = fx.proxy.GraphAppendingTracer(graph)
-
-    x1s = fx.Proxy(graph.placeholder('x1', torch.Tensor), tracer=tracer)
-    x2s = fx.Proxy(graph.placeholder('x2', torch.Tensor), tracer=tracer)
-    weights = fx.Proxy(graph.placeholder('w', torch.Tensor), tracer=tracer)
-
-    empty = fx.Proxy(graph.call_function(torch.empty, ((),), dict(device='cpu')), tracer=tracer)
-    output_shape = torch.broadcast_tensors(empty.expand(x1s.shape[:-1]),
-                                           empty.expand(x2s.shape[:-1]),
-                                           empty.expand(weights.shape[:-1]))[0].shape
-    del empty
-
-    # = Broadcast inputs =
-    x1s, x2s, weights = x1s.broadcast_to(output_shape + (-1,)), \
-                        x2s.broadcast_to(output_shape + (-1,)), \
-                        weights.broadcast_to(output_shape + (-1,))
-
-    output_shape = output_shape + (n_s,)
-
-    x1s = x1s.reshape(-1, n_s)
-    x2s = x2s.reshape(-1, 1)
-    w = weights.reshape((-1, n_s, 1, n_s))
-
-    result = torch.einsum("zuvw,zu,zv->zw", w, x1s, x2s)
-    result = path_weight * result
-    result = result.reshape(output_shape)
-
+def codegen_tensor_product_left_right(n_s):
+    graph   = torch.fx.Graph()
+    tracer  = torch.fx.proxy.GraphAppendingTracer(graph)
+    x1s     = torch.fx.Proxy(graph.placeholder('x1', torch.Tensor), tracer=tracer)
+    weights = torch.fx.Proxy(graph.placeholder('w',  torch.Tensor), tracer=tracer)
+    result  = torch.einsum("zuw,zu->zw", weights.reshape((-1, n_s, n_s)), x1s) / sqrt(n_s)
     graph.output(result.node, torch.Tensor)
     graph.lint()
-
-    constants_root = torch.nn.Module()
-    graphmod = fx.GraphModule(constants_root, graph, class_name="tp_forward")
+    graphmod = torch.fx.GraphModule(torch.nn.Module(), graph, class_name="tp_forward")
     return graphmod
 
 
-
-@compile_mode('script')
-class TensorProduct(CodeGenMixin, torch.nn.Module):
+class TensorProduct(torch.nn.Module):
     def __init__(self, n_s):
-
         super().__init__()
-
-        self._in1_dim = n_s
-        self._in2_dim = 1
-
-        graphmod_left_right = codegen_tensor_product_left_right(n_s, sqrt(1.0/n_s))
-        graphmod_right = fx.Graph()
-        graphmod_right.placeholder('x2', torch.Tensor)
-        graphmod_right.placeholder('w', torch.Tensor)
-        graphmod_right.call_function(
-            torch._assert,
-            args=(False, "`right` method is not compiled, set `compile_right` to True when creating the TensorProduct")
-        )
-        graphmod_right = fx.GraphModule(torch.nn.Module(), graphmod_right, class_name="tp_forward")
-        self._codegen_register({
-            "_compiled_main_left_right": graphmod_left_right,
-            "_compiled_main_right": graphmod_right
-        })
-
+        self.n_s = n_s
         self.weight_numel = n_s * n_s
-        self.register_buffer('weight', torch.Tensor())
-
+        self._compiled_main_left_right = codegen_tensor_product_left_right(n_s)
         output_mask = torch.ones(n_s)
         self.register_buffer('output_mask', output_mask)
+        self.register_buffer('weight', torch.Tensor())
         self._profiling_str = str(self)
 
-
     def forward(self, x, y, weight):
-        assert x.shape[-1] == self._in1_dim, "Incorrect last dimension for x"
-        assert y.shape[-1] == self._in2_dim, "Incorrect last dimension for y"
+        assert x.shape[-1] == self.n_s, "Incorrect last dimension for x"
         assert weight.shape[-1] == self.weight_numel, "Invalid weight shape"
-        print(x.shape[:-1], y.shape[:-1])
-        #print(y.shape)
-        return self._compiled_main_left_right(x, y, weight)
+        return self._compiled_main_left_right(x, weight)
